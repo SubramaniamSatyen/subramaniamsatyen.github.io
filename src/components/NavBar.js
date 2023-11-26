b71d246ca8f5c81417b2dc6e474096d03883ee43
Satyen Subramaniam <66653384+SubramaniamSatyen@users.noreply.github.com>
2023-11-25 20:49:45 -0800

    Adding key to list

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,17 +17,17 @@ const PAGE_MAPPING = {
 function NavBar({dimensions}) {
     return (
         <div>
-            <ul class="nav-menu">
+            <ul className="nav-menu">
                 <div>
                     {dimensions.width > MOBILE_THRESHOLD ? 
-                    <li className='nav-header'>
+                    <li className='nav-header' key="site-name">
                         <Link to="/">Satyen Subramaniam</Link>
                     </li>   
                     : null }
                 </div>
                 {dimensions.width > MOBILE_THRESHOLD ? 
                 <div>
-                    {Object.entries(PAGE_MAPPING).map(([key, value]) => <Link to={value}><li className='nav-item'>{key}</li></Link>)}
+                    {Object.entries(PAGE_MAPPING).map(([key, value]) => <Link to={value} key={value}><li className='nav-item'>{key}</li></Link>)}
                 </div> : 
                 <DropMenu mapping={PAGE_MAPPING}></DropMenu>}
             </ul>
